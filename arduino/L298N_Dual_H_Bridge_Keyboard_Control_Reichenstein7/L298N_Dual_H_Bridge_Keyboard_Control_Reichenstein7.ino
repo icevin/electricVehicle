fbda35c76bdb3cffddb35de36c7173566d0c574a
Kevin Tang <kevintang2023@gmail.com>
2016-12-18 23:20:54 -0500

    Mapped pins to motor controller

--- arduino/L298N_Dual_H_Bridge_Keyboard_Control_Reichenstein7/L298N_Dual_H_Bridge_Keyboard_Control_Reichenstein7.ino
+++ arduino/L298N_Dual_H_Bridge_Keyboard_Control_Reichenstein7/L298N_Dual_H_Bridge_Keyboard_Control_Reichenstein7.ino
@@ -1,26 +1,18 @@
-//Code by Reichenstein7 (thejamerson.com)
 
 //Keyboard Controls:
 //
 // 1 -Motor 1 Left
 // 2 -Motor 1 Stop
 // 3 -Motor 1 Right
-//
-// 4 -Motor 2 Left
-// 5 -Motor 2 Stop
-// 6 -Motor 2 Right
 
-// Declare L298N Dual H-Bridge Motor Controller directly since there is not a library to load.
+int dir1PinA = 8;
+int dir2PinA = 9;
+int speedPinA = 10; // Needs to be a PWM pin to be able to control motor speed
+int controlPin = 2;
+bool toggle = 0;
+bool thisSwitch = 0;
+bool lastSwitch = 0;
 
-// Motor 1
-int dir1PinA = 2;
-int dir2PinA = 3;
-int speedPinA = 9; // Needs to be a PWM pin to be able to control motor speed
-
-// Motor 2
-int dir1PinB = 4;
-int dir2PinB = 5;
-int speedPinB = 10; // Needs to be a PWM pin to be able to control motor speed
 
 void setup() {  // Setup runs once per reset
 // initialize serial communication @ 9600 baud:
@@ -31,16 +23,37 @@ Serial.begin(9600);
 pinMode(dir1PinA,OUTPUT);
 pinMode(dir2PinA,OUTPUT);
 pinMode(speedPinA,OUTPUT);
-pinMode(dir1PinB,OUTPUT);
-pinMode(dir2PinB,OUTPUT);
-pinMode(speedPinB,OUTPUT);
+pinMode(controlPin, INPUT);
 
 }
 
 void loop() {
 
-// Initialize the Serial interface:
 
+
+
+
+lastSwitch = thisSwitch;
+thisSwitch = digitalRead(controlPin); 
+
+if((thisSwitch == 1) && (lastSwitch == 0)) {
+  toggle = !toggle;
+}
+
+
+
+if(toggle) {
+analogWrite(speedPinA, 64);//Sets speed variable via PWM 
+digitalWrite(dir1PinA, LOW);
+digitalWrite(dir2PinA, HIGH);
+} else {
+analogWrite(speedPinA, 0);
+digitalWrite(dir1PinA, LOW);
+digitalWrite(dir2PinA, HIGH);
+}
+
+
+/*
 if (Serial.available() > 0) {
 int inByte = Serial.read();
 int speed; // Local variable
@@ -73,31 +86,6 @@ Serial.println("Motor 1 Reverse");
 Serial.println("   ");
 break;
 
-//______________Motor 2______________
-
-case '4': // Motor 2 Forward
-analogWrite(speedPinB, 255);
-digitalWrite(dir1PinB, LOW);
-digitalWrite(dir2PinB, HIGH);
-Serial.println("Motor 2 Forward");
-Serial.println("   ");
-break;
-
-case '5': // Motor 1 Stop (Freespin)
-analogWrite(speedPinB, 0);
-digitalWrite(dir1PinB, LOW);
-digitalWrite(dir2PinB, HIGH);
-Serial.println("Motor 2 Stop");
-Serial.println("   ");
-break;
-
-case '6': // Motor 2 Reverse
-analogWrite(speedPinB, 255);
-digitalWrite(dir1PinB, HIGH);
-digitalWrite(dir2PinB, LOW);
-Serial.println("Motor 2 Reverse");
-Serial.println("   ");
-break;
 
 default:
 // turn all the connections off if an unmapped key is pressed:
@@ -106,4 +94,7 @@ digitalWrite(thisPin, LOW);
 }
   }
     }
+
+
+    */
       }
